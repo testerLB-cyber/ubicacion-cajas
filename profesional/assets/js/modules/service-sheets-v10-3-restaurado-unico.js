65e2c9d51f0cf8ac0a9b8339a5c925702412b7e8
testerLB-cyber <fjduarteamador@gmail.com>
2026-09-12 12:38:47 -0700

    Activar autocomplete de personas en Hojas v104

--- profesional/assets/js/modules/service-sheets-v10-3-restaurado-unico.js
+++ profesional/assets/js/modules/service-sheets-v10-3-restaurado-unico.js
@@ -3,4 +3,5 @@
   if(window.__HS_V104_LOADER__) return;
   window.__HS_V104_LOADER__=true;
   document.write('<script src="assets/js/modules/service-sheets-v104.js?v=104"><\/script>');
+  document.write('<script src="assets/js/modules/service-sheets-v104-autocomplete.js?v=104"><\/script>');
 })();
