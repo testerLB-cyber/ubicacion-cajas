3268e07989a1216482d89cdba56ba4cc7ad6910a
testerLB-cyber <fjduarteamador@gmail.com>
2026-09-13 12:46:52 -0700

    Hojas v104: integrar unidad y remolque

--- profesional/assets/js/modules/service-sheets-person-v7.js
+++ profesional/assets/js/modules/service-sheets-person-v7.js
@@ -21,6 +21,7 @@
       .then(()=>load('assets/js/modules/service-sheets-list-mode.js?v=correcta1','data-correct-hs-list'))
       .then(()=>load('assets/js/modules/service-sheets-manual-photo-pdf.js?v=correcta1','data-correct-hs-photo'))
       .then(()=>load('assets/js/modules/service-sheets-reopen.js?v=correcta1','data-correct-hs-reopen'))
+      .then(()=>load('assets/js/modules/service-sheets-unit-trailer-v1.js?v=20260913-2','data-hs-unit-trailer-v1'))
       .catch(e=>console.warn('Hojas versionescorrectas1',e));
   });
 })();
