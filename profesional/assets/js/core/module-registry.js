3abfb8187ff3b9696257b2cd91aa32ef414712d3
testerLB-cyber <fjduarteamador@gmail.com>
2026-09-14 14:09:06 -0700

    feat: v49 autocomplete global de catalogos en formularios

--- profesional/assets/js/core/module-registry.js
+++ profesional/assets/js/core/module-registry.js
@@ -2,7 +2,7 @@
 (function(){
   'use strict';
   const app=window.TraficApp=window.TraficApp||{};
-  app.version='profesional-modular-v48-dot-operador-obligatorio-qr-grande';
+  app.version='profesional-modular-v49-autocomplete-global-catalogos';
   app.modules=app.modules||{};
   app.contracts=app.contracts||{};
   app.register=function(name,descriptor){if(!name)throw new Error('Módulo sin nombre');app.modules[name]=Object.assign({name,status:'registered'},descriptor||{});return app.modules[name];};
@@ -35,6 +35,7 @@
     addScript('assets/js/modules/correo-envio-diagnostico-v1.js?v=20260914-mail-diag-1','data-cc-mail-diag');
     addScript('assets/js/modules/correo-validacion-v1.js?v=20260914-mail-validate-1','data-cc-mail-validate');
     addScript('assets/js/modules/dot-operador-borrado-v1.js?v=20260914-dot-required-3','data-cc-dot-op-delete');
+    addScript('assets/js/modules/catalog-autocomplete-global-v1.js?v=20260914-global-catalog-autocomplete-1','data-cc-global-catalog-autocomplete');
     addScript('assets/js/modules/anticipos-utileria-historicos-v1.js?v=20260914-ant-util-hist-1','data-cc-ant-util-hist');
     addScript('assets/js/modules/anticipos-v12-catalogos-flujos.js?v=20260914-ant-catalogos-1','data-cc-ant-catalog-flow');
     addScript('assets/js/modules/anticipos-v12-catalogos-compactos.js?v=20260914-ant-catalogos-selector-2','data-cc-ant-catalog-compact');
