71c964cac007ec2d962d6113583ab910e00a59be
testerLB-cyber <fjduarteamador@gmail.com>
2026-09-14 12:07:21 -0700

    Carga configuración de correo reconstruida

--- profesional/assets/js/core/module-registry.js
+++ profesional/assets/js/core/module-registry.js
@@ -2,7 +2,7 @@
 (function(){
   'use strict';
   const app=window.TraficApp=window.TraficApp||{};
-  app.version='profesional-modular-v42-correo-validado';
+  app.version='profesional-modular-v43-correo-reconstruido';
   app.modules=app.modules||{};
   app.contracts=app.contracts||{};
   app.register=function(name,descriptor){if(!name)throw new Error('Módulo sin nombre');app.modules[name]=Object.assign({name,status:'registered'},descriptor||{});return app.modules[name];};
@@ -32,7 +32,7 @@
     addScript('assets/js/modules/renta-tarifas-tipo-v1.js?v=20260914-rent-commercial-proforma-4','data-cc-rent-type-rates');
     addScript('assets/js/modules/renta-cobro-estabilidad-v1.js?v=20260914-rent-commercial-stable-1','data-cc-rent-commercial-stability');
     addScript('assets/js/modules/correo-envio-diagnostico-v1.js?v=20260914-mail-diag-1','data-cc-mail-diag');
-    addScript('assets/js/modules/correo-validacion-v1.js?v=20260914-mail-validate-1','data-cc-mail-validate');
+    addScript('assets/js/modules/correo-validacion-v2.js?v=20260914-mail-rebuild-1','data-cc-mail-validate-v2');
     addScript('assets/js/modules/anticipos-utileria-historicos-v1.js?v=20260914-ant-util-hist-1','data-cc-ant-util-hist');
     addScript('assets/js/modules/anticipos-v12-catalogos-flujos.js?v=20260914-ant-catalogos-1','data-cc-ant-catalog-flow');
     addScript('assets/js/modules/anticipos-v12-catalogos-compactos.js?v=20260914-ant-catalogos-selector-2','data-cc-ant-catalog-compact');
