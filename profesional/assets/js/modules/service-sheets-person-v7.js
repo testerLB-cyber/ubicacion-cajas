5aea0b21cc3ec970baec9dcba6387362e68468cb
testerLB-cyber <fjduarteamador@gmail.com>
2026-09-22 15:48:32 -0700

    Restore direct QR photo action: service-sheets-person-v7.js

--- profesional/assets/js/modules/service-sheets-person-v7.js
+++ profesional/assets/js/modules/service-sheets-person-v7.js
@@ -20,7 +20,7 @@
       .then(()=>load('assets/js/modules/service-sheets-precapture.js?v=correcta1','data-correct-hs-precap'))
       .then(()=>load('assets/js/modules/service-sheets-show-all.js?v=correcta1','data-correct-hs-all'))
       .then(()=>load('assets/js/modules/service-sheets-list-mode.js?v=correcta1','data-correct-hs-list'))
-      .then(()=>load('assets/js/modules/service-sheets-manual-photo-pdf.js?v=qr-foto-fix-20260922','data-correct-hs-photo'))
+      .then(()=>load('assets/js/modules/service-sheets-manual-photo-pdf.js?v=qr-visible-20260922b','data-correct-hs-photo'))
       .then(()=>load('assets/js/modules/service-sheets-reopen.js?v=correcta1','data-correct-hs-reopen'))
       .then(()=>load('assets/js/modules/service-sheets-unit-trailer-v1.js?v=20260913-2','data-hs-unit-trailer-v1'))
       .catch(e=>console.warn('Hojas versionescorrectas1',e));
