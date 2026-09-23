7a40445cd4ba871d58ba859c14e137d1cc6298aa
testerLB-cyber <fjduarteamador@gmail.com>
2026-09-22 17:11:33 -0700

    Ordenar comprobación de hojas y mostrar foto QR junto a subir imagen

--- profesional/assets/js/modules/service-sheets-person-v7.js
+++ profesional/assets/js/modules/service-sheets-person-v7.js
@@ -14,13 +14,12 @@
     safe('assets/js/modules/anticipos-mobile-catalog-fix.js?v=correcta1','data-correct-ant-catfix');
     safe('assets/js/modules/anticipos-comprobar-unificado-v4.js?v=correcta1','data-correct-ant-unificado');
     window.__HS_V104_AUTOCOMPLETE__=true;
-    load('assets/js/modules/service-sheets-v104.js?v=qr-core-20260923','data-correct-hs-main')
+    load('assets/js/modules/service-sheets-v104.js?v=qr-list-20260923','data-correct-hs-main')
       .then(()=>load('assets/js/modules/service-sheets-any-accepted-custody-v1.js?v=20260914-1','data-hs-any-accepted-custody'))
       .then(()=>load('assets/js/modules/service-sheets-v104-autocomplete.js?v=correcta1','data-correct-hs-auto'))
       .then(()=>load('assets/js/modules/service-sheets-precapture.js?v=correcta1','data-correct-hs-precap'))
       .then(()=>load('assets/js/modules/service-sheets-show-all.js?v=correcta1','data-correct-hs-all'))
-      .then(()=>load('assets/js/modules/service-sheets-list-mode.js?v=qr-core-20260923','data-correct-hs-list'))
-      .then(()=>load('assets/js/modules/service-sheets-manual-photo-pdf.js?v=qr-core-20260923','data-correct-hs-photo'))
+      .then(()=>load('assets/js/modules/service-sheets-manual-photo-pdf.js?v=qr-list-20260923','data-correct-hs-photo'))
       .then(()=>load('assets/js/modules/service-sheets-reopen.js?v=correcta1','data-correct-hs-reopen'))
       .then(()=>load('assets/js/modules/service-sheets-unit-trailer-v1.js?v=20260913-2','data-hs-unit-trailer-v1'))
       .catch(e=>console.warn('Hojas versionescorrectas1',e));
