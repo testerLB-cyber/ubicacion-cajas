be2243037de1b5f32cc6051469c52e008f64943e
testerLB-cyber <fjduarteamador@gmail.com>
2026-09-23 20:41:18 -0700

    Fix actual service sheets loader to load direct check button

--- profesional/assets/js/modules/service-sheets-person-v7.js
+++ profesional/assets/js/modules/service-sheets-person-v7.js
@@ -20,7 +20,7 @@
       .then(()=>load('assets/js/modules/service-sheets-precapture.js?v=20260923-comp-v2','data-correct-hs-precap'))
       .then(()=>load('assets/js/modules/service-sheets-show-all.js?v=correcta1','data-correct-hs-all'))
       .then(()=>load('assets/js/modules/service-sheets-manual-photo-pdf.js?v=20260923-editqr-stable17','data-correct-hs-photo'))
-      .then(()=>load('assets/js/modules/service-sheets-unit-trailer-v1.js?v=20260913-2','data-hs-unit-trailer-v1'))
+      .then(()=>load('assets/js/modules/service-sheets-unit-trailer-v1.js?v=20260924-directcheck2','data-hs-unit-trailer-v1'))
       .catch(e=>console.warn('Hojas versionescorrectas1',e));
   });
 })();
